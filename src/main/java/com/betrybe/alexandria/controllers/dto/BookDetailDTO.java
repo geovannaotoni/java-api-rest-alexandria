ac0775c271631d32f9c8bbcfc10c5c2513d572df
Geovanna Otoni <geovannaotoni@gmail.com>
2023-12-20 16:05:15 -0300

    fix: corrige nome do BookDetailDto

--- src/main/java/com/betrybe/alexandria/controllers/dto/BookDetailDTO.java
+++ src/main/java/com/betrybe/alexandria/controllers/dto/BookDetailDTO.java
@@ -3,8 +3,8 @@ package com.betrybe.alexandria.controllers.dto;
 import com.betrybe.alexandria.models.entities.BookDetail;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public record BookDetailDTO(Long id, String sumary, @JsonProperty("page_count") Integer pageCount, String year, String isbn) {
+public record BookDetailDTO(Long id, String summary, @JsonProperty("page_count") Integer pageCount, String year, String isbn) {
   public BookDetail toBookDetail() {
-    return new BookDetail(id, sumary, pageCount, year, isbn, null);
+    return new BookDetail(id, summary, pageCount, year, isbn, null);
   }
 }
